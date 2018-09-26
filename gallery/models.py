55f5b648c91352fe21ff0d2b2ebf1f5c1d1c56a6
Owenmur21 <owenmuriithi@gmail.com>
2018-09-26 14:39:34 +0300

    Creates Location class

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    """
+    Class that contains location details of the image posted
+    """
+    name = models.CharField(max_length = 15)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
