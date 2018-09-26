2f89c5e98a26e5fda0fe8a43f3574cb8f10aa894
Owenmur21 <owenmuriithi@gmail.com>
2018-09-26 15:48:42 +0300

    Creates test to check if imagea re being saved

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import Location, Category, Image
 
-# Create your tests here.
+
+class ImageTestClass(TestCase):
+    """
+    Tests Image class and its functions
+    """
+    #Set up method
+    def setUp(self):
+        self.image = Image(photo='test.jpg', name='testing pic', description='test times at testcase')
+
+    #Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image, Image))
+
+    def test_save_method(self):
+        """
+        Test for saving uploaded images
+        """
+        self.image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0 )
