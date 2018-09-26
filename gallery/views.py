079735a5059e676d15dfcde7ee128a19aee9f214
Owenmur21 <owenmuriithi@gmail.com>
2018-09-26 10:37:37 +0300

    Creates home template

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,10 @@
 from django.http  import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('Ozzy gallery')
+    """
+    Function that renders the landing page
+    """
+    return render(request, 'home.html')
 
 
