85a298aa9540f89070a70f965e2fb2a5c8238531
Owenmur21 <owenmuriithi@gmail.com>
2018-09-27 22:01:34 +0300

    Calls seaching function

--- gallery/views.py
+++ gallery/views.py
@@ -9,3 +9,18 @@ def home(request):
     images = Image.get_images()
     return render(request, 'home.html', {"images":images})
 
+def search_image(request):
+    """
+    Function that searches images by category
+    """
+    if 'image' in request.GET and request.GET["image"]:
+        category = request.GET.get("image")
+        searched_images = Image.search_image(category)
+        message = f"{category}"
+        
+        return render(request, 'search.html', {"message":message, "images":searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message":message})
+
