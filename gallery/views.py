d1ed238dc509778edb8741b34703b7d3190d602a
Owenmur21 <owenmuriithi@gmail.com>
2018-09-27 15:21:17 +0300

    Adds details to the home function

--- gallery/views.py
+++ gallery/views.py
@@ -1,10 +1,11 @@
 from django.http  import HttpResponse
 from django.shortcuts import render
+from .models import Image
 
 def home(request):
     """
     Function that renders the landing page
     """
-    return render(request, 'home.html')
-
+    images = Image.get_images()
+    return render(request, 'home.html', {"images":images})
 
