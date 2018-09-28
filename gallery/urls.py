aa7c32a62fc270fccd989ac68745d96c7a6d68d3
Owenmur21 <owenmuriithi@gmail.com>
2018-09-28 11:22:10 +0300

    Creates location route

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     url('^$', views.home, name='welcome'),
     url(r'category/', views.search_image, name='search_image'),
+    url(r'location/(\d+)', views.filter_by_location, name='location'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
