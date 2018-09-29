cb7ae031376e72e1570b860f77640f5129527b34
Owenmur21 <owenmuriithi@gmail.com>
2018-09-29 21:46:30 +0300

    Deployment

--- collage/settings.py
+++ collage/settings.py
@@ -1,5 +1,4 @@
 import os
-import django_heroku
 import dj_database_url
 from decouple import config, Csv
 MODE=config("MODE", default="dev")
@@ -148,10 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-django_heroku.settings(locals())
+
