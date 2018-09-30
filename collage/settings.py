17ecaa877f09618e7e9c2196dc51eef86ebc1ea9
Owenmur21 <owenmuriithi@gmail.com>
2018-09-30 10:40:04 +0300

    Checks

--- collage/settings.py
+++ collage/settings.py
@@ -86,7 +86,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
-                'django.template.context_processors.media',
+                
             ],
         },
     },
@@ -132,6 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 django_heroku.settings(locals())
